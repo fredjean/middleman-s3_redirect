03ed13f61b199dca6485657aa39075d434090c08
Andrew Kvalheim <Andrew@Kvalhe.im>
2014-10-10 11:18:01 -0700

    Use credentials from environment variables, if available.

--- lib/middleman-s3_redirect/extension.rb
+++ lib/middleman-s3_redirect/extension.rb
@@ -15,6 +15,9 @@ module Middleman
 
       def initialize
         self.read_config
+
+        self.aws_access_key_id ||= ENV['AWS_ACCESS_KEY_ID']
+        self.aws_secret_access_key ||= ENV['AWS_SECRET_ACCESS_KEY']
       end
 
       def redirect(from, to)
